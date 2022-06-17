3c54ee0a0016eaf377619b209dc42759778e10f4
AlanJSimpson <allanstark2908@gmail.com>
2022-06-16 22:02:07 -0300

    Ajustando projeto para porcentagem em um unico grafico

--- src/components/proc-chart/ProcChart.tsx
+++ src/components/proc-chart/ProcChart.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import FileSaver from 'file-saver';
 import { useCallback } from 'react';
 
@@ -31,7 +31,6 @@ export default function ProcChart({
   total: number;
 }): JSX.Element {
   const [getPng, { ref: barChart, isLoading }] = useCurrentPng();
-  const [getPercentagePng, { ref: percentageChart }] = useCurrentPng();
 
   const handleDownload = useCallback(async () => {
     const png = await getPng();
@@ -41,53 +40,25 @@ export default function ProcChart({
     }
   }, [getPng]);
 
-  const handleDownloadPng = useCallback(async () => {
-    const png = await getPercentagePng();
-
-    if (png) {
-      FileSaver.saveAs(png, 'proc-percentage-chart.png');
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [getPercentagePng]);
-
   const data = [
     {
       name: 'H. Comunicativas',
       'Pontuação máxima': HC_CONSTANTE,
-      'Pontuação alcançada': HC,
-    },
-    {
-      name: 'C. Linguagem Oral',
-      'Pontuação máxima': CLO_CONSTANTE,
-      'Pontuação alcançada': CLO,
-    },
-    {
-      name: 'A.D. Cognitivo',
-      'Pontuação máxima': ADC_CONSTANTE,
-      'Pontuação alcançada': ADC,
-    },
-    {
-      name: 'Total',
-      'Pontuação máxima': TOTAL_CONSTANTE,
-      'Pontuação alcançada': total,
-    },
-  ];
-
-  const porcentageData = [
-    {
-      name: 'H. Comunicativas',
       'Pontuação alcançada': Math.round((HC * 100) / HC_CONSTANTE),
     },
     {
       name: 'C. Linguagem Oral',
+      'Pontuação máxima': CLO_CONSTANTE,
       'Pontuação alcançada': Math.round((CLO * 100) / CLO_CONSTANTE),
     },
     {
       name: 'A.D. Cognitivo',
+      'Pontuação máxima': ADC_CONSTANTE,
       'Pontuação alcançada': Math.round((ADC * 100) / CLO_CONSTANTE),
     },
     {
       name: 'Total',
+      'Pontuação máxima': TOTAL_CONSTANTE,
       'Pontuação alcançada': Math.round((total * 100) / TOTAL_CONSTANTE),
     },
   ];
@@ -97,26 +68,10 @@ export default function ProcChart({
       <BarChart width={730} height={250} data={data} ref={barChart}>
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis dataKey='name' />
-        <YAxis />
+        <YAxis tickFormatter={(tick, index) => index * 25 + '%'} />
         <Tooltip />
         <Legend />
         <Bar dataKey='Pontuação máxima' fill='#AB4A30' />
-        <Bar dataKey='Pontuação alcançada' fill='#D46A40' />
-      </BarChart>
-
-      <Divider />
-
-      <BarChart
-        width={730}
-        height={250}
-        data={porcentageData}
-        ref={percentageChart}
-      >
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='name' />
-        <Tooltip />
-        <Legend />
-        <YAxis tickFormatter={(tick) => tick + '%'} />
         <Bar dataKey='Pontuação alcançada' fill='#D46A40' />
       </BarChart>
 
@@ -126,13 +81,6 @@ export default function ProcChart({
         onClick={handleDownload}
       >
         {isLoading ? 'Downloading...' : 'Download proc'}
-      </Button>
-      <Button
-        className='download-png-button'
-        variant='contained'
-        onClick={handleDownloadPng}
-      >
-        {isLoading ? 'Downloading...' : 'Download porcentagem proc'}
       </Button>
     </Box>
   );
